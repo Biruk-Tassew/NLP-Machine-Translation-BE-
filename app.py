afc3f49bd7877288975fe32f0fc06ecc73733006
Abdulaziz Ali <variant.abdulaziz@gmail.com>
2024-01-12 22:43:01 +0300

    add model

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import tensorflow as tf
+import tensorflow_text as text
 
 app = Flask(__name__)
 CORS(app)
@@ -15,10 +17,12 @@ def translate():
     return jsonify({'english_translation': english_translation})
 
 def translate_amharic_to_english(amharic_sentence):
-    # Implement your translation logic here
-    english_translation = f"Translated: {amharic_sentence}"
+    model = tf.saved_model.load("./translator")
+    result = model(tf.constant(amharic_sentence))
+    english_translation = result.numpy()[0].decode('utf-8')
 
     return english_translation
 
 if __name__ == '__main__':
     app.run(debug=False)
+
